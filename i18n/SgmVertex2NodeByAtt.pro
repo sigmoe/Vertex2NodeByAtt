3bb0fc38c2945458ae7880e7c653f13a7969e6c7
Etienne MORO - SIGMOÉ <55922863+sigmoe@users.noreply.github.com>
2020-05-04 09:27:03 +0200

    Add files via upload
    v3.02

---
+++ i18n/SgmVertex2NodeByAtt.pro
@@ -0,0 +1,4 @@
+FORMS = ../gui/sgm_vertex2nodebyatt_dlgparams.ui
+SOURCES = ../sgm_vertex2nodebyatt.py \
+          ../sgm_vertex2nodebyatt_tr.py
+TRANSLATIONS = SgmVertex2NodeByAtt_fr.ts
---
+++ i18n/SgmVertex2NodeByAtt_fr.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>SgmVertex2NodeByAtt</name>
+    <message>
+        <location filename="../sgm_vertex2nodebyatt.py" line="85"/>
+        <source>SIGMOE</source>
+        <translation>Sigmoé</translation>
+    </message>
+    <message>
+        <location filename="../sgm_vertex2nodebyatt.py" line="86"/>
+        <source>Moves lines to points by attribute</source>
+        <translation>Déplace lignes sur points par attribut</translation>
+    </message>
+    <message>
+        <location filename="../sgm_vertex2nodebyatt.py" line="87"/>
+        <source>Process stopped</source>
+        <translation>Arrêt du traitement</translation>
+    </message>
+    <message>
+        <location filename="../sgm_vertex2nodebyatt.py" line="88"/>
+        <source>Move completed</source>
+        <translation>Déplacement terminé</translation>
+    </message>
+    <message>
+        <location filename="../sgm_vertex2nodebyatt.py" line="89"/>
+        <source>Layer %s is not editable</source>
+        <translation>La couche %s n&apos;est pas modifiable</translation>
+    </message>
+    <message>
+        <location filename="../sgm_vertex2nodebyatt.py" line="90"/>
+        <source>Field %s is not usable</source>
+        <translation>Le champ %s n&apos;est pas exploitable</translation>
+    </message>
+</context>
+<context>
+    <name>dlgParams</name>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="26"/>
+        <source>Moving lines vertices to points</source>
+        <translation>Déplacement des sommets de lignes sur les points (ou inversement)</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="80"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;QGIS plugin developped by SIGMOÉ © 2020&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-style:italic;&quot;&gt;Extension QGIS développée par SIGMOÉ © 2020&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="111"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://sigmoe.fr&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;www.sigmoe.fr&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://sigmoe.fr&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;www.sigmoe.fr&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="160"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="198"/>
+        <source>Line layer containing the lines :</source>
+        <translation>Couche linéaire contenant les lignes :</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="226"/>
+        <source>Lines field containing the reference of the attached points :</source>
+        <translation>Champ des lignes contenant la référence des points rattachés :</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="254"/>
+        <source>Reference separator in the lines field :</source>
+        <translation>Séparateur des références dans le champ des lignes :</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="289"/>
+        <source>Point layer containing the points :</source>
+        <translation>Couche ponctuelle contenant les points :</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="317"/>
+        <source>Points field containing the reference :</source>
+        <translation>Champ des points contenant la référence :</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="345"/>
+        <source>Object movement mode :</source>
+        <translation>Mode de déplacement des objets :</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="358"/>
+        <source>the lines are moved to the points</source>
+        <translation>les lignes sont déplacées sur les points</translation>
+    </message>
+    <message>
+        <location filename="../gui/sgm_vertex2nodebyatt_dlgparams.ui" line="374"/>
+        <source>the points are moved along the line</source>
+        <translation>les points sont déplacés sur les lignes</translation>
+    </message>
+</context>
+</TS>
